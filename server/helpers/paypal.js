fbe4f0e3df7e7f7ac57ac6fb7351442a21f73c05
AbySunny <106875320+AbySunny@users.noreply.github.com>
2025-05-08 09:16:37 +0530

    Update paypal.js

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -2,8 +2,8 @@ const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
   mode: "sandbox", // Use "live" for production
-  client_id: "AayQIPHgUn3Hd7IVfZlRIxktKh1LBuKFFIUEpIsNhDvwVNz2f2c_ImsqRDcSP3Ufv6PyDZOGmMbs-A95",  // Replace with your PayPal Client ID
-  client_secret: "EAOEG2SO61yjJh5HIzcH9iYveBjINu8QbQ_to59E3Ekhfz0wuLOYYolU-sBGMsBOZDUzUTIQ10Rf97PA",  // Replace with your PayPal Client Secret
+  client_id: "...",  // Replace with your PayPal Client ID
+  client_secret: "...",  // Replace with your PayPal Client Secret
 });
 
 module.exports = paypal;
